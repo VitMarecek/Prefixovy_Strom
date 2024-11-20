2179b919b595dfdd969e6fd60b9e9d03d6176a3c
Vít Mareček <vitecek282@gmail.com>
2024-11-20 22:52:39 +0100

    Začátek kódu
    Začátek kódu a nalezení testovacího Frameworku

--- Prefix_Tree/Prefix_Tree/Form1.cs
+++ Prefix_Tree/Prefix_Tree/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Prefix_Tree
 {
     public partial class Form1 : Form
@@ -15,6 +16,31 @@ namespace Prefix_Tree
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void Pridat(string slovo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<String> Hledat(string hledatString)
+        {
+            return new string[] { };
+        }
+
+        protected class PrefixovyStromUzel
+        {
+            private readonly Dictionary<char, PrefixovyStromUzel> podUzly;
+            private bool jeSlovo;
+
+            public PrefixovyStromUzel()
+            {
+                podUzly = new Dictionary<char, PrefixovyStromUzel>();
+                jeSlovo = false;
+            }
+
+            public Dictionary<char, PrefixovyStromUzel> PodUzly { get { return podUzly; } }
+            public bool JeSlovo { get { return JeSlovo; } set { jeSlovo = value; } }
         }
     }
 }
